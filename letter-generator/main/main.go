ade6bda630ced06785772fc5e9574cb6cb4747ff
Ryan Berger <ryanbberger@gmail.com>
2018-07-19 17:52:53 -0600

    add worker pattern

--- letter-generator/main/main.go
+++ letter-generator/main/main.go
@@ -10,23 +10,18 @@ import (
 	"unicode"
 	"image/color/palette"
 	"github.com/ryan-berger/shia-birthday/letter-generator"
+	"sync"
 )
 
 type generatorInfo struct {
-	letter   string
-	filler   *gif.GIF
-	content  *gif.GIF
+	letter  string
+	filler  *gif.GIF
+	content *gif.GIF
+	index   int
 }
 
-const A = `
-SHHHS
-SHSHS
-SHHHS
-SHSHS
-SHSHS
-`
 
-func main()  {
+func main() {
 	shiaFile, _ := os.Open("images/shia.gif")
 	shiaHeadFile, _ := os.Open("images/shiaHead.gif")
 
@@ -36,57 +31,61 @@ func main()  {
 	shiaGif, _ := gif.DecodeAll(shiaFile)
 	shiaHeadGif, _ := gif.DecodeAll(shiaHeadFile)
 
-	finishedChan := make(chan interface{}, 26)
+	workerChannel := make(chan *generatorInfo)
+	waitGroup := &sync.WaitGroup{}
 
-	for i := 0; i < 26 ; i++ {
-		generateLetter(&generatorInfo{
-			letter: letter_generator.Alphabet[i],
-			filler: shiaGif,
-			content: shiaHeadGif,
-		}, finishedChan)
-
+	for i := 0; i < 10; i++ {
+		waitGroup.Add(1)
+		go generateLetter(workerChannel, waitGroup)
 	}
 
 	for i := 0; i < 26; i++ {
-		select {
-			case <- finishedChan:
-				fmt.Print("finished")
+		workerChannel <- &generatorInfo{
+			letter: letter_generator.Alphabet[i],
+			filler: shiaGif,
+			content: shiaHeadGif,
+			index: i,
 		}
 	}
+
+	waitGroup.Wait()
 }
 
-
-func generateLetter(info *generatorInfo, finished chan interface{}) {
-	trimmed := strings.Map(func(r rune) rune {
-		if unicode.IsSpace(r) {
-			return -1
-		}
-		return r
-	}, info.letter)
-
-	letterGif := &gif.GIF{}
-
-	for i := 0; i < 60; i++ {
-		newImage := image.NewPaletted(image.Rect(0, 0, 640, 640), palette.Plan9)
-
-
-		for j := 0; j < 5; j++ {
-			for k := 0; k < 5; k++ {
-				adjustedIndex := (j * 5) + k
-				var selectedGif *image.Paletted
-
-				if trimmed[adjustedIndex] == 72 {
-					selectedGif = info.filler.Image[i]
-				} else {
-					selectedGif = info.content.Image[i]
-				}
-
-				draw.Draw(newImage, image.Rect(k * 128, j * 128, (k* 128) + 128, (j * 128) + 128), selectedGif, image.ZP, draw.Over)
+func generateLetter(work chan *generatorInfo, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for info := range work {
+		trimmed := strings.Map(func(r rune) rune {
+			if unicode.IsSpace(r) {
+				return -1
 			}
+			return r
+		}, info.letter)
+
+		letterGif := &gif.GIF{}
+
+		for i := 0; i < 60; i++ {
+			newImage := image.NewPaletted(image.Rect(0, 0, 640, 640), palette.Plan9)
+
+			for j := 0; j < 5; j++ {
+				for k := 0; k < 5; k++ {
+					adjustedIndex := (j * 5) + k
+					var selectedGif *image.Paletted
+					if rune(trimmed[adjustedIndex]) == rune('-') {
+						selectedGif = info.filler.Image[i]
+					} else {
+						selectedGif = info.content.Image[i]
+					}
+
+					draw.Draw(newImage, image.Rect(k*128, j*128, (k*128)+128, (j*128)+128), selectedGif, image.ZP, draw.Over)
+				}
+			}
+
+			fmt.Println(fmt.Sprintf("finished cycle #%d", i))
+
+			letterGif.Image = append(letterGif.Image, newImage)
+			letterGif.Delay = append(letterGif.Delay, 0)
 		}
-
-		letterGif.Image = append(letterGif.Image, newImage)
+		f, _ := os.Create(fmt.Sprintf("letter-generator/letters/%d.gif", info.index))
+		gif.EncodeAll(f, letterGif)
 	}
-
-
 }
