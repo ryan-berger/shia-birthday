f327818ed90d1e339f17e6054417f5528fa84178
Ryan Berger <ryanbberger@gmail.com>
2018-07-19 18:26:38 -0600

    reduced allocations

--- letter-generator/main/main.go
+++ letter-generator/main/main.go
@@ -63,6 +63,14 @@ func main() {
 }
 
 func generateLetter(work chan *generatorInfo, wg *sync.WaitGroup) {
+	letterGif := &gif.GIF{}
+
+	for i := 0; i < 60; i++ {
+		letterGif.Image = append(letterGif.Image, image.NewPaletted(image.Rect(0, 0, 640, 640), palette.Plan9))
+		letterGif.Delay = append(letterGif.Delay, 0)
+	}
+
+
 	defer wg.Done()
 	for info := range work {
 		trimmed := strings.Map(func(r rune) rune {
@@ -72,11 +80,7 @@ func generateLetter(work chan *generatorInfo, wg *sync.WaitGroup) {
 			return r
 		}, info.letter)
 
-		letterGif := &gif.GIF{}
-
 		for i := 0; i < 60; i++ {
-			newImage := image.NewPaletted(image.Rect(0, 0, 640, 640), palette.Plan9)
-
 			for j := 0; j < 5; j++ {
 				for k := 0; k < 5; k++ {
 					adjustedIndex := (j * 5) + k
@@ -87,14 +91,10 @@ func generateLetter(work chan *generatorInfo, wg *sync.WaitGroup) {
 						selectedGif = info.content.Image[i]
 					}
 
-					draw.Draw(newImage, image.Rect(k*128, j*128, (k*128)+128, (j*128)+128), selectedGif, image.ZP, draw.Over)
+					draw.Draw(letterGif.Image[i], image.Rect(k*128, j*128, (k*128)+128, (j*128)+128), selectedGif, image.ZP, draw.Over)
 				}
 			}
-
-			fmt.Println(fmt.Sprintf("finished cycle #%d", i))
-
-			letterGif.Image = append(letterGif.Image, newImage)
-			letterGif.Delay = append(letterGif.Delay, 0)
+			fmt.Println(fmt.Sprintf("letter: %d cycle: %d", info.index, i))
 		}
 		f, _ := os.Create(fmt.Sprintf("letter-generator/letters/%d.gif", info.index))
 		gif.EncodeAll(f, letterGif)
